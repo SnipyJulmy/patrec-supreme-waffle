8c930337eef0666d8d4ddd76410ab03a6d4f29d3
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-04-28 19:28:19 +0200

    update

--- s04/main.py
+++ s04/main.py
@@ -6,15 +6,14 @@ import numpy as np
 import csv
 import operator
 
-redo_preprocess = False
+redo_preprocess = True
 folder_unprocessed = "PatRec17_KWS_Data"
-lst = ['270','271','272','273','274','275','276','277','278','279','300','301','302','303','304']
-
+lst = ['270', '271', '272', '273', '274', '275', '276', '277', '278', '279', '300', '301', '302', '303', '304']
 
 if redo_preprocess:
     # Crop images + use OTSU
     print("------------- Preprocess image (crop and OTSU)")
-    preprocess.svg_to_data(folder_unprocessed,lst)
+    preprocess.svg_to_data(folder_unprocessed, lst)
     print("------------- End of preprocessing")
     print("-------------")
 print("------------- Create training dictionnary")
@@ -25,7 +24,7 @@ print("------------- End of creation of the training dictionnary")
 keyword = open(folder_unprocessed + "/task/keywords.txt").read().splitlines()
 validation_dict = data.create_set('data/valid')
 
-transcriptions_dict = data.get_transcriptions(folder_unprocessed+'/ground-truth/transcription.txt')
+transcriptions_dict = data.get_transcriptions(folder_unprocessed + '/ground-truth/transcription.txt')
 
 output_lines = []
 for key, image in validation_dict.items():
@@ -36,16 +35,16 @@ for key, image in validation_dict.items():
     output_line = [key]
     score_dict = {}
     i = 0
-    for key2, image2 in training_dict.items():      
-          
+    for key2, image2 in training_dict.items():
+
         score = dtw_o.calculate_cost(image2)
         score_dict[key2] = score
         i += 1
-        if i%100 == 0:
+        if i % 100 == 0:
             print(i)
         break
-    sorted_score = dict(sorted(score_dict.items(), key=operator.itemgetter(1)))
-    for name,score in sorted_score.items():
+    sorted_score = dict(sorted(score_dict.items(), key = operator.itemgetter(1)))
+    for name, score in sorted_score.items():
         output_line.append(name)
         output_line.append(score)
     output_lines.append(output_line)
