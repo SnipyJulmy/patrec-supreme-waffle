6531aecb4f03b22d7a87aa0bbb19fd38cea4fe5c
Sylvain Julmy <sylvain.julmy@gmail.com>
2019-04-28 20:22:17 +0200

    update

--- s04/main.py
+++ s04/main.py
@@ -6,7 +6,9 @@ import numpy as np
 import csv
 import operator
 
-redo_preprocess = True
+from itertools import islice
+
+redo_preprocess = False
 folder_unprocessed = "PatRec17_KWS_Data"
 lst = ['270', '271', '272', '273', '274', '275', '276', '277', '278', '279', '300', '301', '302', '303', '304']
 
@@ -26,29 +28,29 @@ validation_dict = data.create_set('data/valid')
 
 transcriptions_dict = data.get_transcriptions(folder_unprocessed + '/ground-truth/transcription.txt')
 
+print("Number of element in the validation dict: %d" % len(validation_dict))
+print("Total number of computation : %d" % len(validation_dict) * len(training_dict))
+
+validation_dict = dict(islice(validation_dict.items(), 10))
+
 output_lines = []
 for key, image in validation_dict.items():
-    print(key)
-    if key != '300-12-01':
-        continue
+    print('process %s' % (key))
     dtw_o = DTW(image)
     output_line = [key]
     score_dict = {}
     i = 0
     for key2, image2 in training_dict.items():
-
         score = dtw_o.calculate_cost(image2)
         score_dict[key2] = score
         i += 1
-        if i % 100 == 0:
-            print(i)
-        break
+        if i % 10 == 0:
+            break
     sorted_score = dict(sorted(score_dict.items(), key = operator.itemgetter(1)))
     for name, score in sorted_score.items():
         output_line.append(name)
         output_line.append(score)
     output_lines.append(output_line)
-    break
 with open("output3.csv", "w") as f:
     writer = csv.writer(f)
     writer.writerows(output_lines)
